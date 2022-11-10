74b145f579b18ba8f3b585a2051fe1ed42a7f5cd
Ana Moros <ana.morosmarmol@sky.uk>
2022-11-10 14:19:20 +0000

    first commit, file structure

---
+++ src/models/User.js
@@ -0,0 +1,12 @@
+//import our db, Model, DataTypes
+const { DataTypes } = require("sequelize");
+const db = require("../db/db");
+
+//Creating a User child class from the Model parent class
+const User = db.define("users", {
+  username: DataTypes.STRING,
+  password: DataTypes.STRING,
+});
+
+//exports
+module.exports = { User };
